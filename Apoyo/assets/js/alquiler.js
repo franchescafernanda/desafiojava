7bef3d8f91dd498ed128e5ade3c3f73c72599df8
franchescafernanda <franchescafer26@gmail.com>
2024-01-14 10:37:39 -0300

    Add: Information in index.htm & js

---
+++ Apoyo/assets/js/alquiler.js
@@ -0,0 +1,81 @@
+const propiedades_alquiler = [
+    {
+        name: 'Apartamento en el centro de la ciudad',
+        src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        description: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        adrrees: '123 Main Street, Anytown, CA 91234',
+        rooms: 2,
+        bathrooms: 2,
+        price: 2000,
+        smoke: false,
+        pets: true
+    },
+    {
+        name: 'Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        description: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        adrrees: '456 Ocean Avenue, Seaside Town, CA 56789',
+        rooms: 3,
+        bathrooms: 3,
+        costo: 2500,
+        smoke: true,
+        pets: true
+    },
+    {
+        name: 'Condominio moderno en zona residencial',
+        src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        description: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+        adrrees: '123 Main Street, Anytown, CA 91234',
+        rooms: 2,
+        bathrooms: 2,
+        costo: 2200,
+        smoke: false,
+        pets: false
+    },
+    {
+        Name: 'Cabaña Santa Linda',
+        src: 'https://www.cabanaslosquenes.cl/wp-content/gallery/santa-linda/Imagen-de-WhatsApp-2023-03-20-a-las-12.28.21.jpg', /*preguntar como hacer esto*/ /*era el url de las img*/
+        Description: 'Cabaña en localidad rural',
+        Addrees: 'Camino a Los Quenes Km 36 Los Queñes, Maule',
+        Rooms: '2',
+        Bathrooms: '1',
+        Price: '4000',
+        Smoke: true,
+        pets: true,
+    },
+    {
+        Name: 'Cabaña Volcán descabezado',
+        src: 'https://www.cabanaslosquenes.cl/wp-content/gallery/volcan-descabezado/WhatsApp-Image-2023-03-20-at-11.30.38.jpeg', /*preguntar como hacer esto*/
+        Description: 'Cabaña construida totalmente de madera natural, con hermosa vista al Volcán.',
+        Addrees: 'Camino a Los Quenes Km 36 Los Queñes, Maule',
+        Rooms: '2',
+        Bathrooms: '1',
+        Price: '4500',
+        Smoke: true,
+        pets: true,
+    },
+    {
+        Name: 'Cabaña Cerro Cayetano',
+        src: 'https://www.cabanaslosquenes.cl/wp-content/gallery/home_1/2.jpg', /*preguntar como hacer esto*/
+        Description: 'Esta hermosa cabaña consta de un ambiente, con piscina y áreas verdes.',
+        Addrees: '123 Main Street, Anytown, CA 91234',
+        Rooms: '1',
+        Bathrooms: '1',
+        Price: '4200',
+        Smoke: false,
+        pets: false,
+    },
+    {
+        name: 'Cabaña Volcán Panchon',
+        src: 'https://www.cabanaslosquenes.cl/wp-content/gallery/volcan-planchon/WhatsApp-Image-2023-03-20-at-11.29.20.jpeg', /*preguntar como hacer esto*/
+        description: 'Hermosa cabaña consta de: Estufa a pellet, Directv, Parrilla, Cocina completa, Sabanas',
+        addrees: 'Chillán, Campos de hielo',
+        Rooms: '2',
+        Bathrooms: '1',
+        Price: '3000',
+        Smoke: true,
+        pets: true,
+    }
+]
+
+export default propiedades_alquiler
---
+++ Apoyo/assets/js/index.js
@@ -0,0 +1,6 @@
+/*import {propiedades_ventas} from './ventas'
+console.log (propiedades_ventas)*/
+
+import propiedades_alquiler from './alquiler.js'
+import propiedades_ventas from './ventas.js'
+console.log (propiedades_ventas)
