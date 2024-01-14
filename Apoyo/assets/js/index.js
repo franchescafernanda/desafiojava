b037d9b626b6509b32b4663a1857ad23d19baeb8
guerreroron <guerreroron@hotmail.com>
2024-01-14 13:37:56 -0300

    aplicando cambios en alquiler y ventas (falta el index)

--- Apoyo/assets/js/index.js
+++ Apoyo/assets/js/index.js
@@ -1,6 +1,24 @@
-/*import {propiedades_ventas} from './ventas'
-console.log (propiedades_ventas)*/
+import propiedades_ventas from './ventas.js'
+console.log('propiedades_ventas --->', propiedades_ventas);
 
 import propiedades_alquiler from './alquiler.js'
-import propiedades_ventas from './ventas.js'
-console.log (propiedades_ventas)
+console.log('propiedades_alquiler --->', propiedades_alquiler);
+
+
+
+
+const venta3 = propiedades_ventas.slice(1,4)
+console.log('venta3 --->', venta3);
+
+const alquiler3 = propiedades_alquiler.slice(1,4)
+console.log('alquiler3 --->', alquiler3);
+
+
+
+
+const ventaIndex = document.getElementById('propiedades-venta-lista')
+
+const alquilerIndex = document.getElementById('propiedades-alquiler-lista')
+
+
+
